767fe2bb0220eb64ef2afd62e7923c4906d409f4
Francesco Biscani <bluescarni@gmail.com>
2023-09-14 17:08:26 +0200

    More files.

---
+++ include/tanuki/tanuki.hpp
@@ -0,0 +1,12 @@
+// Copyright 2023 Francesco Biscani (bluescarni@gmail.com)
+//
+// This file is part of the tanuki library.
+//
+// This Source Code Form is subject to the terms of the Mozilla
+// Public License v. 2.0. If a copy of the MPL was not distributed
+// with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#ifndef TANUKI_TANUKI_HPP
+#define TANUKI_TANUKI_HPP
+
+#endif

cc4cc374a93aecb815e0552b8c56f279cdc140d8
Francesco Biscani <bluescarni@gmail.com>
2023-09-30 15:05:33 +0200

    Initial OSX attempt.

---
+++ tools/gha_osx_clang_14.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+# Echo each command
+set -x
+
+# Exit on error.
+set -e
+
+# Core deps.
+sudo apt-get install wget
+
+# Install conda+deps.
+wget https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-MacOSX-x86_64.sh -O miniconda.sh
+export deps_dir=$HOME/local
+export PATH="$HOME/miniconda/bin:$PATH"
+bash miniconda.sh -b -p $HOME/miniconda
+mamba create -y -q -p $deps_dir 'clang=14.*' 'clangxx=14.*' cmake boost-cpp ninja
+source activate $deps_dir
+
+# Create the build dir and cd into it.
+mkdir build
+cd build
+
+# GCC build.
+cmake -G Ninja ../ -DCMAKE_PREFIX_PATH=$deps_dir -DCMAKE_BUILD_TYPE=Debug -DTANUKI_BUILD_TESTS=yes -DTANUKI_WITH_BOOST_S11N=yes
+ninja
+ctest -V -j4
+
+set +e
+set +x

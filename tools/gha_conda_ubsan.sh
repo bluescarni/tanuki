01d40320ea5b0e358003f93c5a3b12ea543cdf06
Francesco Biscani <bluescarni@gmail.com>
2023-09-30 22:36:06 +0200

    Try gcc.

--- tools/gha_conda_ubsan.sh
+++ tools/gha_conda_ubsan.sh
@@ -14,7 +14,7 @@ wget https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge
 export deps_dir=$HOME/local
 export PATH="$HOME/miniconda/bin:$PATH"
 bash miniconda.sh -b -p $HOME/miniconda
-mamba create -y -q -p $deps_dir cxx-compiler clangxx cmake ninja
+mamba create -y -q -p $deps_dir cxx-compiler cmake ninja
 source activate $deps_dir
 
 # Create the build dir and cd into it.
@@ -22,7 +22,7 @@ mkdir build
 cd build
 
 # GCC build.
-cmake -G Ninja ../ -DCMAKE_PREFIX_PATH=$deps_dir -DCMAKE_CXX_FLAGS="-fsanitize=undefined" -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_BUILD_TYPE=Debug -DTANUKI_BUILD_TESTS=yes -DTANUKI_WITH_BOOST_S11N=yes
+cmake -G Ninja ../ -DCMAKE_PREFIX_PATH=$deps_dir -DCMAKE_CXX_FLAGS="-fsanitize=undefined" -DCMAKE_CXX_COMPILER=g++ -DCMAKE_BUILD_TYPE=Debug -DTANUKI_BUILD_TESTS=yes -DTANUKI_WITH_BOOST_S11N=yes
 ninja
 ctest -V -j4
 

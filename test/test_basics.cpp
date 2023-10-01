6af4a8a0658a36c19b708aa64b7b6a54e40e68e1
Francesco Biscani <bluescarni@gmail.com>
2023-10-01 21:47:29 +0200

    More testing.

--- test/test_basics.cpp
+++ test/test_basics.cpp
@@ -25,6 +25,7 @@
 
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wmissing-field-initializers"
+#pragma GCC diagnostic ignored "-Wself-move"
 
 #endif
 
@@ -149,6 +150,10 @@ TEST_CASE("basics")
     REQUIRE(is_invalid(w2));
     // NOLINTEND
 
+    // Emplace test with class which is not copyable/movable.
+    const wrap_t w_mut(tanuki::emplace<std::mutex>);
+    REQUIRE(noexcept(wrap_t(tanuki::emplace<int>)));
+
     auto w4 = w3;
 
     // NOLINTBEGIN
@@ -169,10 +174,121 @@ TEST_CASE("basics")
     w2 = wrap_t(large{});
     auto w2a = wrap_t(large{});
     w2 = w2a;
+}
 
-    const wrap_t w(tanuki::emplace<std::mutex>);
+TEST_CASE("assignment")
+{
+    using tanuki::wrap;
+    using wrap_t = wrap<any_iface>;
 
-    REQUIRE(noexcept(wrap_t(tanuki::emplace<int>)));
+    wrap_t w(42);
+
+    // Self assign, copy and move.
+    REQUIRE_NOTHROW(w = *&w);
+    // NOLINTNEXTLINE
+    REQUIRE_NOTHROW(w = std::move(*&w));
+
+    // Object revival via copy assignment.
+    {
+        wrap_t wl1(large{});
+        auto wl2 = std::move(wl1);
+        // NOLINTNEXTLINE
+        REQUIRE(is_invalid(wl1));
+        const wrap_t wl3(large{{}, "briffo"});
+        wl1 = wl3;
+        REQUIRE(!is_invalid(wl1));
+        REQUIRE(value_ref<large>(wl1).str == "briffo");
+    }
+
+    {
+        wrap_t wl1(small{});
+        auto wl2 = std::move(wl1);
+        // NOLINTNEXTLINE
+        REQUIRE(!is_invalid(wl1));
+        const wrap_t wl3(small{"briffo"});
+        wl1 = wl3;
+        REQUIRE(!is_invalid(wl1));
+        REQUIRE(value_ref<small>(wl1).s == "briffo");
+    }
+
+    // Object revival via move assignment.
+    {
+        wrap_t wl1(large{});
+        auto wl2 = std::move(wl1);
+        // NOLINTNEXTLINE
+        REQUIRE(is_invalid(wl1));
+        wrap_t wl3(large{{}, "briffo"});
+        wl1 = std::move(wl3);
+        REQUIRE(!is_invalid(wl1));
+        // NOLINTNEXTLINE
+        REQUIRE(is_invalid(wl3));
+        REQUIRE(value_ref<large>(wl1).str == "briffo");
+    }
+
+    {
+        wrap_t wl1(small{});
+        auto wl2 = std::move(wl1);
+        // NOLINTNEXTLINE
+        REQUIRE(!is_invalid(wl1));
+        wrap_t wl3(small{"briffo"});
+        wl1 = std::move(wl3);
+        REQUIRE(!is_invalid(wl1));
+        // NOLINTNEXTLINE
+        REQUIRE(!is_invalid(wl3));
+        REQUIRE(value_ref<small>(wl1).s == "briffo");
+    }
+
+    // Copy-assignment with different types.
+    {
+        wrap_t w1(small{}), w2(large{.str = "blaf"});
+        w1 = w2;
+        REQUIRE(value_ref<large>(w1).str == "blaf");
+        REQUIRE(!is_invalid(w2));
+    }
+
+    // Move-assignment with different types.
+    {
+        wrap_t w1(small{}), w2(large{.str = "blaf"});
+        w1 = std::move(w2);
+        REQUIRE(value_ref<large>(w1).str == "blaf");
+        // NOLINTNEXTLINE
+        REQUIRE(is_invalid(w2));
+    }
+
+    // Copy-assignment with same types.
+    {
+        wrap_t w1(large{.str = "blif"}), w2(large{.str = "blaf"});
+        w1 = w2;
+        REQUIRE(value_ref<large>(w1).str == "blaf");
+        REQUIRE(!is_invalid(w2));
+    }
+    {
+        wrap_t w1(small{.s = "blif"}), w2(small{.s = "blaf"});
+        w1 = w2;
+        REQUIRE(value_ref<small>(w1).s == "blaf");
+        REQUIRE(!is_invalid(w2));
+    }
+
+    // Move-assignment with same types.
+    {
+        wrap_t w1(large{.str = "blif"}), w2(large{.str = "blaf"});
+        const auto *p1 = value_ptr<large>(w1);
+        const auto *p2 = value_ptr<large>(w2);
+        w1 = std::move(w2);
+        REQUIRE(value_ref<large>(w1).str == "blaf");
+        // NOLINTNEXTLINE
+        REQUIRE(!is_invalid(w2));
+        // Check value ptr swapping.
+        REQUIRE(value_ptr<large>(w1) == p2);
+        REQUIRE(value_ptr<large>(w2) == p1);
+    }
+    {
+        wrap_t w1(small{.s = "blif"}), w2(small{.s = "blaf"});
+        w1 = std::move(w2);
+        REQUIRE(value_ref<small>(w1).s == "blaf");
+        // NOLINTNEXTLINE
+        REQUIRE(!is_invalid(w2));
+    }
 }
 
 TEST_CASE("basics2")

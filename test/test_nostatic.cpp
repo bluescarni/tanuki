5cb3dc6328e1b7859a4f9b8b26ac27caf1294417
Francesco Biscani <bluescarni@gmail.com>
2023-10-02 19:54:25 +0200

    Forgot swapping, try codecov fix.

--- test/test_nostatic.cpp
+++ test/test_nostatic.cpp
@@ -238,6 +238,86 @@ TEST_CASE("assignment")
     }
 }
 
+TEST_CASE("swap")
+{
+    using std::swap;
+
+    using wrap_t = tanuki::wrap<any_iface, tanuki::config<>{.static_size = 0}>;
+
+    // NOLINTNEXTLINE
+    REQUIRE(std::is_nothrow_swappable_v<wrap_t>);
+
+    // Self swap.
+    {
+        wrap_t w{3};
+
+        REQUIRE_NOTHROW(swap(w, w));
+    }
+
+    // Swap invalid values.
+    {
+        wrap_t w1{large{}}, w1a(std::move(w1));
+        wrap_t w2{large{}}, w2a(std::move(w2));
+
+        swap(w1, w2);
+    }
+
+    // Revive via swapping.
+    {
+        wrap_t w1{large{}}, w1a(std::move(w1));
+        wrap_t w2{large{.str = "blif"}};
+
+        REQUIRE(is_invalid(w1));
+
+        swap(w1, w2);
+
+        REQUIRE(!is_invalid(w1));
+        REQUIRE(value_ref<large>(w1).str == "blif");
+    }
+
+    {
+        wrap_t w2{large{}}, w2a(std::move(w2));
+        wrap_t w1{large{.str = "blif"}};
+
+        REQUIRE(is_invalid(w2));
+
+        swap(w1, w2);
+
+        REQUIRE(!is_invalid(w2));
+        REQUIRE(value_ref<large>(w2).str == "blif");
+    }
+
+    // Swap different types.
+    {
+        wrap_t w1{small{.s = "blif"}}, w2{large{.str = "blaf"}};
+        swap(w1, w2);
+        REQUIRE(value_ref<large>(w1).str == "blaf");
+        REQUIRE(value_ref<small>(w2).s == "blif");
+    }
+
+    // Swap same types.
+    {
+        wrap_t w1{small{.s = "blif"}}, w2{small{.s = "blaf"}};
+        const auto *p1 = value_ptr<small>(w1);
+        const auto *p2 = value_ptr<small>(w2);
+        swap(w1, w2);
+        REQUIRE(value_ref<small>(w1).s == "blaf");
+        REQUIRE(value_ref<small>(w2).s == "blif");
+        REQUIRE(value_ptr<small>(w1) == p2);
+        REQUIRE(value_ptr<small>(w2) == p1);
+    }
+    {
+        wrap_t w1{large{.str = "blif"}}, w2{large{.str = "blaf"}};
+        const auto *p1 = value_ptr<large>(w1);
+        const auto *p2 = value_ptr<large>(w2);
+        swap(w1, w2);
+        REQUIRE(value_ref<large>(w1).str == "blaf");
+        REQUIRE(value_ref<large>(w2).str == "blif");
+        REQUIRE(value_ptr<large>(w1) == p2);
+        REQUIRE(value_ptr<large>(w2) == p1);
+    }
+}
+
 // NOLINTEND(cert-err58-cpp,misc-use-anonymous-namespace,cppcoreguidelines-avoid-do-while)
 
 #if defined(__GNUC__)

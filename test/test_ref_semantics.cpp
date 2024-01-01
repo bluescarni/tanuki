883b8c7c2664e84c4bb86f5131f6115116ce24a1
Francesco Biscani <bluescarni@gmail.com>
2024-01-01 13:39:26 +0100

    More testing WIP.

--- test/test_ref_semantics.cpp
+++ test/test_ref_semantics.cpp
@@ -1,5 +1,6 @@
 #include <concepts>
 #include <functional>
+#include <type_traits>
 
 #include <tanuki/tanuki.hpp>
 
@@ -62,6 +63,28 @@ TEST_CASE("basics")
     REQUIRE(!is_invalid(w4));
     REQUIRE(value_isa<std::reference_wrapper<const foo>>(w4));
     REQUIRE(value_ref<std::reference_wrapper<const foo>>(w4).get().value == 43);
+
+    // Generic in-place constructor.
+    const wrap2_t w5(tanuki::in_place<int>, 11);
+    REQUIRE(!is_invalid(w5));
+    REQUIRE(value_isa<int>(w5));
+    REQUIRE(value_ref<int>(w5) == 11);
+
+    // Copy construction.
+    REQUIRE(std::is_nothrow_copy_constructible_v<wrap2_t>);
+    auto w6(w5);
+    REQUIRE(!is_invalid(w6));
+    REQUIRE(value_isa<int>(w6));
+    REQUIRE(value_ref<int>(w6) == 11);
+    REQUIRE(tanuki::value_ptr<int>(w5) == tanuki::value_ptr<int>(w6));
+
+    // Move construction.
+    REQUIRE(std::is_nothrow_copy_constructible_v<wrap2_t>);
+    auto w7(std::move(w6));
+    REQUIRE(!is_invalid(w7));
+    REQUIRE(value_isa<int>(w7));
+    REQUIRE(value_ref<int>(w7) == 11);
+    REQUIRE(tanuki::value_ptr<int>(w5) == tanuki::value_ptr<int>(w7));
 }
 
 // NOLINTEND(cert-err58-cpp,misc-use-anonymous-namespace,cppcoreguidelines-avoid-do-while)

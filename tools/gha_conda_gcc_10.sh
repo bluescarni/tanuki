b59ca0822492712d52e6d7071b413dc4ae528224
Francesco Biscani <bluescarni@gmail.com>
2024-02-25 11:21:53 +0100

    Revert "More specific GCC 10 pin."
    This reverts commit 7aee27c33e3eb806c88d98d9d409bc826f94bc29.

--- tools/gha_conda_gcc_10.sh
+++ tools/gha_conda_gcc_10.sh
@@ -14,9 +14,7 @@ wget https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge
 export deps_dir=$HOME/local
 export PATH="$HOME/miniconda/bin:$PATH"
 bash miniconda.sh -b -p $HOME/miniconda
-# NOTE: let's pin to 10.2 since that is the version
-# currently in use on manylinux2014.
-mamba create -y -q -p $deps_dir 'gxx=10.2.*' cmake libboost-devel ninja
+mamba create -y -q -p $deps_dir 'gxx=10.*' cmake libboost-devel ninja
 source activate $deps_dir
 
 # Create the build dir and cd into it.

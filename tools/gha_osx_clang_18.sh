784323fb3f11f8a6f43bccce01c0cdc894f84426
Francesco Biscani <bluescarni@gmail.com>
2024-04-01 14:46:49 +0200

    Try adding clang 18 build on osx.

---
+++ tools/gha_osx_clang_18.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+# Echo each command
+set -x
+
+# Exit on error.
+set -e
+
+# Install conda+deps.
+wget https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-MacOSX-x86_64.sh -O miniconda.sh
+export deps_dir=$HOME/local
+export PATH="$HOME/miniconda/bin:$PATH"
+bash miniconda.sh -b -p $HOME/miniconda
+mamba create -y -q -p $deps_dir 'clangxx=18.*' cmake libboost-devel ninja
+source activate $deps_dir
+
+# Create the build dir and cd into it.
+mkdir build
+cd build
+
+# GCC build.
+cmake -G Ninja ../ -DCMAKE_PREFIX_PATH=$deps_dir \
+    -DCMAKE_CXX_STANDARD=23 \
+    -DCMAKE_CXX_COMPILER=clang++ \
+    -DCMAKE_BUILD_TYPE=Debug \
+    -DTANUKI_BUILD_TESTS=yes \
+    -DTANUKI_BUILD_TUTORIALS=yes \
+    -DTANUKI_WITH_BOOST_S11N=yes
+ninja
+ctest -V -j4
+
+set +e
+set +x

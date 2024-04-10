6b24b203fb5cee14fc5beb90cb168737ec34beab
Francesco Biscani <Francesco.Biscani@ext.esa.int>
2024-04-10 16:06:59 +0000

    Testing additions.

--- test/test_subrange.cpp
+++ test/test_subrange.cpp
@@ -12,7 +12,22 @@
 
 // NOLINTBEGIN(cert-err58-cpp,misc-use-anonymous-namespace,cppcoreguidelines-avoid-do-while)
 
-TEST_CASE("basics")
+// NOTE: the purpose of this test is to check that
+// the range type erasure machinery works properly
+// when type-erasing non-type-erased ranges that return
+// type-erased iterators (say that three times).
+//
+// Before fixing the implementation of type-erased ranges,
+// the following would happen:
+// - no type-erasure would take place in the begin()
+//   method of generic_range due to the use of make_generic_iterator(),
+//   which would detect that the user-defined iterator is already
+//   type-erased and which would thus return a copy of the user-defined
+//   iterator;
+// - type-erasure would however always happen in the end() implementation,
+//   and this would break the logic in the sentinel methods that need to
+//   access the user-defined iterator stored in the wrap.
+TEST_CASE("type erased subranges")
 {
     std::vector<int> v = {1, 2, 3};
 
@@ -27,6 +42,20 @@ TEST_CASE("basics")
         auto r1 = facade::make_random_access_range(std::ref(v));
         auto r2 = std::ranges::subrange(r1.begin(), r1.end());
         auto r3 = facade::make_random_access_range(std::ref(r2));
+        REQUIRE(std::ranges::equal(r3, v));
+    }
+
+    {
+        const auto r1 = facade::make_random_access_range(std::cref(v));
+        auto r2 = std::ranges::subrange(r1.begin(), r1.begin() + 2);
+        const auto r3 = facade::make_random_access_range(std::cref(r2));
+        REQUIRE(std::ranges::equal(r3, r2));
+    }
+
+    {
+        const auto r1 = facade::make_random_access_range(std::cref(v));
+        auto r2 = std::ranges::subrange(r1.begin(), r1.end());
+        const auto r3 = facade::make_random_access_range(std::cref(r2));
         REQUIRE(std::ranges::equal(r3, v));
     }
 }

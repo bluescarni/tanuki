42e244f4d9fcd2817a6b8b6503890cac730fdeda
Francesco Biscani <bluescarni@gmail.com>
2024-05-15 18:01:26 +0200

    Add testing for nonintrusive interface implementations.

---
+++ test/test_nonintrusive.cpp
@@ -0,0 +1,89 @@
+#include <concepts>
+
+#include <tanuki/tanuki.hpp>
+
+#include <catch2/catch_test_macros.hpp>
+
+// NOLINTBEGIN(cert-err58-cpp,misc-use-anonymous-namespace,cppcoreguidelines-avoid-do-while)
+
+namespace ns
+{
+
+struct my_iface {
+    virtual ~my_iface() = default;
+    virtual int foo() const = 0;
+};
+
+// Also include an interface with an impl template
+// to check the non-intrusive representation takes
+// the precedence.
+struct my_iface2 {
+    virtual ~my_iface2() = default;
+    virtual int bar() const = 0;
+
+    template <typename Base, typename Holder, typename T>
+    struct impl {
+    };
+};
+
+} // namespace ns
+
+namespace tanuki
+{
+
+// Empty default nonintrusive implementation.
+template <typename Base, typename Holder, typename T>
+struct iface_impl<ns::my_iface, Base, Holder, T> {
+};
+
+// Specialisation for int value type.
+template <typename Base, typename Holder>
+struct iface_impl<ns::my_iface, Base, Holder, int> : public Base {
+    int foo() const final
+    {
+        return 42;
+    }
+};
+
+template <typename Base, typename Holder, typename T>
+struct iface_impl<ns::my_iface2, Base, Holder, T> {
+};
+
+// Specialisation for int value type.
+template <typename Base, typename Holder>
+struct iface_impl<ns::my_iface2, Base, Holder, int> : public Base {
+    int bar() const final
+    {
+        return 43;
+    }
+};
+
+} // namespace tanuki
+
+TEST_CASE("basics")
+{
+    using wrap_t = tanuki::wrap<ns::my_iface>;
+
+    wrap_t w(123);
+    REQUIRE(w->foo() == 42);
+
+    REQUIRE(!std::constructible_from<wrap_t, long>);
+
+    using wrap2_t = tanuki::wrap<ns::my_iface2>;
+
+    wrap2_t w2(123);
+    REQUIRE(w2->bar() == 43);
+
+    REQUIRE(!std::constructible_from<wrap2_t, long>);
+
+    // Composite interface.
+    using wrap3_t = tanuki::wrap<tanuki::composite_iface<ns::my_iface, ns::my_iface2>>;
+
+    wrap3_t w3(123);
+    REQUIRE(w3->foo() == 42);
+    REQUIRE(w3->bar() == 43);
+
+    REQUIRE(!std::constructible_from<wrap3_t, long>);
+}
+
+// NOLINTEND(cert-err58-cpp,misc-use-anonymous-namespace,cppcoreguidelines-avoid-do-while)

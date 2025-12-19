1457565644ca65a840d465d43097e8985ae3ab13
Francesco Biscani <bluescarni@gmail.com>
2025-12-19 12:56:40 +0100

    More debugging.

--- test/test_lib_s11n.cpp
+++ test/test_lib_s11n.cpp
@@ -1,9 +1,11 @@
 #include <boost/archive/binary_oarchive.hpp>
+#include <iostream>
 #include <sstream>
 
 #include <catch2/catch_test_macros.hpp>
 
 #include "fooable.hpp"
+#include "tanuki/tanuki.hpp"
 
 // NOLINTBEGIN(cert-err58-cpp,misc-use-anonymous-namespace,cppcoreguidelines-avoid-do-while)
 
@@ -13,6 +15,8 @@ TEST_CASE("test s11n")
 {
     fooable::foo_wrap<int> f{fooable::foo_model{.n = 5}};
 
+    std::cout << "Constructed!" << std::endl;
+
     std::stringstream ss;
 
     {
@@ -20,12 +24,17 @@ TEST_CASE("test s11n")
         oa << f;
     }
 
+    std::cout << "Serialised!" << std::endl;
+
     value_ref<fooable::foo_model>(f).n = 0;
 
     {
         boost::archive::binary_iarchive ia(ss);
         ia >> f;
     }
+
+    std::cout << "Deserialised!" << std::endl;
+    std::cout << "The internal value type is: " << tanuki::demangle(value_type_index(f).name()) << std::endl;
 
     REQUIRE(value_ref<fooable::foo_model>(f).n == 5);
 }

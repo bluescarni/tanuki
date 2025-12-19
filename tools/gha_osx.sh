0f6aca0ad887223aee14d1e371e99e66e2324be5
Francesco Biscani <bluescarni@gmail.com>
2025-12-19 14:08:31 +0100

    Try specific clang version.

--- tools/gha_osx.sh
+++ tools/gha_osx.sh
@@ -11,7 +11,7 @@ wget https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge
 export deps_dir=$HOME/local
 export PATH="$HOME/miniconda/bin:$PATH"
 bash miniconda.sh -b -p $HOME/miniconda
-mamba create -y -q -p $deps_dir c-compiler cxx-compiler cmake libboost-devel ninja
+mamba create -y -q -p $deps_dir c-compiler cxx-compiler 'clangxx=20.*' cmake libboost-devel ninja
 source activate $deps_dir
 
 # Create the build dir and cd into it.
